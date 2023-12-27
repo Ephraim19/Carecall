6792a792a99435c5740c29da11a1daa2c28f3f20
Ephraim19 <machayoephraim@gmail.com>
2023-12-27 21:58:36 +0300

    Updates

---
+++ precache-manifest.5e819784375777f43e933acaccb5db8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4d208a566c99896552d",
+    "url": "/Carecall/static/css/main.2ee89114.chunk.css"
+  },
+  {
+    "revision": "e4d208a566c99896552d",
+    "url": "/Carecall/static/js/main.e4d208a5.chunk.js"
+  },
+  {
+    "revision": "f7afa9f5281624826d3e",
+    "url": "/Carecall/static/js/1.f7afa9f5.chunk.js"
+  },
+  {
+    "revision": "993f7790a2222f798cc2",
+    "url": "/Carecall/static/css/2.bbcd1648.chunk.css"
+  },
+  {
+    "revision": "993f7790a2222f798cc2",
+    "url": "/Carecall/static/js/2.993f7790.chunk.js"
+  },
+  {
+    "revision": "ede62b8a40ca29f63e19",
+    "url": "/Carecall/static/js/runtime~main.ede62b8a.js"
+  },
+  {
+    "revision": "edfb00de9a78d49ad8ac73d231a46295",
+    "url": "/Carecall/index.html"
+  }
+];
